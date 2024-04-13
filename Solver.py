cadde464c678b3c89e5f97037e5e12c6ddbf68b9
dodgero11 <khangpro216@gmail.com>
2024-04-13 10:39:05 +0700

    fix dumb mistakes

--- Solver.py
+++ Solver.py
@@ -14,10 +14,10 @@ def PL_Resolution(sentence: CNFSentence):
     new_CNF_to_copy = CNFSentence(temp_first_CNF)
     temp_second_CNF = new_CNF_to_copy.__sentence__()
     while(True):
-        for i in range(new_CNF_to_copy.__len__()):
+        for i in range(len(temp_second_CNF)):
             if (stop == True):
                 break
-            for j in range(sentence.__len__()):
+            for j in range(len(temp_first_CNF)):
                 is_resolvable, temp_i, temp_j = temp_second_CNF[i].is_resolvable_with(temp_first_CNF[j])
                 if (is_resolvable == True):
                     new_clause = temp_second_CNF[i].resolve(temp_first_CNF[j], temp_i, temp_j)
@@ -27,29 +27,28 @@ def PL_Resolution(sentence: CNFSentence):
                         if (len(new_clause.__literals__()) == 0):
                             big_string += '\n' + "{}"
                             stop = True
+                            new_second_CNF.append(new_clause)
                             break
                         else: 
                             big_string += '\n' + new_clause.formula()
                             new_second_CNF.append(new_clause)
-        if (len(new_second_CNF) == 0):
-            print(len(new_second_CNF), end = '')
-            print("NO")
-            return
         if (stop == True):
             print(len(new_second_CNF), end = '')
             print(big_string)
             print("YES")
             return
+        if (len(new_second_CNF) == 0):
+            print(len(new_second_CNF))
+            print("NO")
+            return
         print(len(new_second_CNF), end = '')
         print(big_string)
-        if (first_iteration != True):
-            for clause in temp_second_CNF:
-                temp_first_CNF.append(clause)
+        for clause in new_second_CNF:
+            temp_first_CNF.append(clause)
         new_CNF_to_copy = CNFSentence(new_second_CNF)
         temp_second_CNF = deepcopy(new_CNF_to_copy.__sentence__())
         new_second_CNF = []
         big_string = ""
-        first_iteration = False
 
 def is_duplicated(sentence, clause):
     for other_clause in sentence:
