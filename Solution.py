140d14b575733430f954a70f332e1daf3a6a72da
ro11 <khangpro216@gmail.com>
2024-04-15 01:11:26 +0700

    update inputs

--- Solution.py
+++ Solution.py
@@ -2,7 +2,7 @@ from CNFComponents import CNFSentence
 from FileReader import CNFFileReader
 from Solver import PL_Resolution
 
-sentence = CNFFileReader("input2.txt")
+sentence = CNFFileReader("input.txt")
 CNFs = sentence.readSentence()
 
 PL_Resolution(CNFs)
